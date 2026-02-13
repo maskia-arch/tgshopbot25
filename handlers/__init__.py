0faeb96a6a460a24aa4a862a7f1a2d3a9ed3786b
maskia-arch <mail.skibba@gmail.com>
2026-02-13 15:26:28 +0100

    update 0.0.134

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,13 +2,14 @@ from .admin_handlers import router as admin_router
 from .customer_handlers import router as customer_router
 from .payment_handlers import router as payment_router
 from .shop_settings import router as settings_router
+from .master_admin_handlers import router as master_admin_router
 
-# Wir bündeln alle Router in einer Liste für einen einfachen Import in der main.py
 all_handlers = [
     admin_router,
     customer_router,
     payment_router,
-    settings_router
+    settings_router,
+    master_admin_router
 ]
 
 __all__ = [
@@ -16,5 +17,6 @@ __all__ = [
     "customer_router",
     "payment_router",
     "settings_router",
+    "master_admin_router",
     "all_handlers"
 ]
